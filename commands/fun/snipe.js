a31c1e125b6aa81820f8f688c06a68e545cca18c
Michael Cole <mike@S-Te-18-182.local>
2020-06-21 21:04:37 +0200

    019238019283019820391820398109238109283021983

--- commands/fun/snipe.js
+++ commands/fun/snipe.js
@@ -7,7 +7,7 @@ module.exports = {
         const msg = client.snipes.get(message.channel.id)
         if (!msg) return message.channel.send("There is nothing to snipe!")
         const embed = new Discord.MessageEmbed()
-            .setAuthor(msg.author + "'s message has been sniped!")
+            .setAuthor(msg.author)
             .setDescription(msg.content)
             .setTimestamp()
         if (msg.image) embed.setImage(msg.image)
