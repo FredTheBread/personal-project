87cb5351c185505a4fdecfd12c3c09086a3647e9
Michael Cole <mike@S-Te-18-182.local>
2020-06-26 21:52:19 +0200

    01928301928309182309aosidj

--- commands/fun/math.js
+++ commands/fun/math.js
@@ -6,18 +6,24 @@ module.exports = {
     category: "fun",
     description: "Returns latency and API ping",
     run: async (client, message, args) => {
-        if(!args[0]) return message.reply('Please input a calcualation.');
+
+        if (!args[0]) return message.channel.send('Please provide a question');
+
         let resp;
+
         try {
-            resp = math.eval(args.join(' '));
+            resp = math.evaluate(args.join(" "))
         } catch (e) {
-            return message.reply('Sorry, but please input a valid calculation')
+            return message.channel.send('Please provide a **valid** question')
         }
+
         const embed = new Discord.MessageEmbed()
-        .setColor(0xffffff)
-        .setTitle('Math calculation')
-        .addField('Input', `\`\`\`js\n${args.join('')}\`\`\``)
-        .addField('Output', `\`\`\`js\n${resp}\`\`\``)
-        message.channel.send(embed)
+            .setColor(0x808080)
+            .setTitle('Calculator')
+            .addField('Question', `\`\`\`css\n${args.join(' ')}\`\`\``)
+            .addField('Answer', `\`\`\`css\n${resp}\`\`\``)
+
+        message.channel.send(embed);
+
     }
 }
