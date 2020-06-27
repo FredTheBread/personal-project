d1c9de4258c48c97a2f6f519cd2aae50e621caca
Michael Cole <mike@S-Te-18-182.local>
2020-06-27 10:40:05 +0200

    iasojdoiasjd01928301298

--- commands/fun/bot.js
+++ commands/fun/bot.js
@@ -3,7 +3,7 @@ module.exports = {
     category: "fun",
     description: "Returns latency and API ping",
     run: async (client, message, args) => {
-        if (!args[1]) return message.channel.send('Please specify a bot ID!')
-        message.channel.send(`https://discordapp.com/oauth2/authorize?client_id=${args[1]}&scope=bot`)
+        if (!args[0]) return message.channel.send('Please specify a bot ID!')
+        message.channel.send(`https://discordapp.com/oauth2/authorize?client_id=${args[0]}&scope=bot`)
     }
 }
