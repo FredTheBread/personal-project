f8b63f440abd0933e1c968c86f0ae00f950e4ae1
Michael Cole <mike@S-Te-18-182.local>
2020-07-02 15:41:56 +0200

    102983019joasdj

---
+++ commands/moderation/lockdown.js
@@ -0,0 +1,16 @@
+let lockit = require('l')
+
+module.exports = {
+    name: "lockdown",
+    category: "moderation",
+    description: "Lockdown a server",
+    run: async (client, message, args) => {
+        if (!client.lockit) client.lockit = [];
+        if (!message.member.hasPermission("MANAGE_CHANNELS")) return message.reply("<a:b_no:721969465205588048> | You don't have the permission to do that!");
+
+        message.channel.createOverwrite(message.guild.id, {
+            SEND_MESSAGES: false
+        })
+        message.channel.send(`**${message.author.username}** locked down this channel.`);
+    }
+}
