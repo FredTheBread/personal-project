c313307a1931fb655f28036ba0b66d14f87832e3
Michael Cole <mike@S-Te-18-182.local>
2020-07-03 15:24:56 +0200

    apsidjo19283091283

--- commands/owner/guild.js
+++
@@ -1,9 +0,0 @@
-const Discord = require("discord.js");
-module.exports = {
-    name: "createguild",
-    category: "owner",
-    description: "Creates a server",
-    run: async (client, message, args, channels) => {
-
-    }
-}
