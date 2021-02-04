439b34c688d651eaf78e296c7ddd3feeb2d216d6
FredTheBread <58191276+FredTheBread@users.noreply.github.com>
2021-02-04 21:06:55 +0100

    50

--- commands/moderation/ban.js
+++ commands/moderation/ban.js
@@ -16,16 +16,19 @@ module.exports = {
         if (!message.guild.me.hasPermission("BAN_MEMBERS")) {
             return message.channel.send(`**${message.author.username}**, I do not have enough permission to use this command`);
         }
-
         let target = message.mentions.members.first();
-        let reason = args.slice(1).join(" ")
+        
+        if(!target.bannable) {
+            return message.channel.send("This user cannot be banned!")
+        }
+        let reason = args.slice(1).join(" ");
         if (!target) {
             return message.channel.send(`**${message.author.username}**, please mention the person who you want to ban`)
         }
         if (target.id === message.author.id) {
             return message.channel.send(`**${message.author.username}**, you can\'t kick yourself`)
         }
-        if (!reason) reason = "No reason provided"
+        if (!reason) reason = "No reason provided.";
         let embed = new Discord.MessageEmbed()
             .setTitle(`Ban Hammer`)
             .setDescription(`Banned ${target} (${target.id})`)
