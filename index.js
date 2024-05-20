5a50acd7ab0aff7077c49c609fb5fd3f8622ff07
Mike <58191276+FredTheBread@users.noreply.github.com>
2024-05-20 19:01:12 +0200

    Update index.js

--- index.js
+++ index.js
@@ -224,7 +224,7 @@ client.on('guildCreate', guild => {
 });
 
 client.on("message", async (message) => {
-    Levels.setURL = (`mongodb+srv://FredTheBread:<password>@discord.uziev.mongodb.net/<dbname>?retryWrites=true&w=majority`);
+    Levels.setURL = (``);
     if (!message.guild) return;
     if (message.author.bot) return;
 
